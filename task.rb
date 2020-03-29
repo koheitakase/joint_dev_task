03f8c936afb20c8258905c376aeee98630fd0d65
Kohei Takase <yasaiseikatu0306@gmail.com>
2020-03-29 17:57:20 +0900

    q15_2 answer

--- task.rb
+++ task.rb
@@ -137,8 +137,9 @@ def q15
   data2 = { name: "yamada", hobby: "baseball", role: "normal" }
 
   # 以下に回答を記載
-  p data1.include?(:age)
-  p data2.include?(:age)
+  
+  puts data1.include?(:age)? "OK" : "NG"
+  puts data2.include?(:age)? "OK" : "NG"
 end
 
 def q16
